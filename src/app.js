b940b8e1c650093e8ae19273d125136a2675a130
Leonardo <leonardo02lima@gmail.com>
2025-07-08 02:27:04 -0300

    feat: implementação de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
+
 const express = require('express');
-const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+  origin: '*', // ou use o domínio do seu front-end: 'https://meuapp.vercel.app'
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
 app.use('/auth', authRoutes);
 
 module.exports = app;
