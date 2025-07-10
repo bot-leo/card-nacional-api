4b5a94b76fa9eda513c009fdcde7be89c20fcd38
Leonardo <leonardo02lima@gmail.com>
2025-07-09 21:42:44 -0300

    feat: login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
 const authService = require('../services/authService');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 class AuthController {
   async register(req, res) {
@@ -9,6 +12,33 @@ class AuthController {
       res.status(400).json({ error: err.message });
     }
   }
+
+  login = async (req, res) => {
+  const { email, senha } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: 'Usuário não encontrado' });
+
+    const isMatch = await bcrypt.compare(senha, user.senha);
+    if (!isMatch) return res.status(400).json({ error: 'Senha incorreta' });
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    const { nomeCompleto, cpf, dataNascimento, plano } = user;
+
+    res.status(200).json({
+      token,
+      user: { nomeCompleto, cpf, dataNascimento, email, plano }
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao realizar login' });
+  }
+};
 }
 
 module.exports = new AuthController();
---
+++ src/middleWares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: 'Token não fornecido' });
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select('-password');
+    if (!user) return res.status(401).json({ error: 'Usuário não encontrado' });
+
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token inválido' });
+  }
+};
+
+module.exports = authMiddleware;
