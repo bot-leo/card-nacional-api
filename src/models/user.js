c64ea8ccacfe42f00d83f68dcc5e3e4d2d8b925c
Leonardo <leonardo02lima@gmail.com>
2025-07-23 14:17:51 -0300

    feat: implementation useSyncExternal

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { useSyncExternalStore } = require('react');
 
 const UserSchema = new mongoose.Schema({
   nomeCompleto: { type: String, required: true },
@@ -6,6 +7,6 @@ const UserSchema = new mongoose.Schema({
   dataNascimento: { type: Date, required: true },
   email: { type: String, required: true, unique: true },
   senha: { type: String, required: true },
-}, { timestamps: true });
+}, { timestamps: true });useSyncExternalStore
 
 module.exports = mongoose.model('User', UserSchema);
